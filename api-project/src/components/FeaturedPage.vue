c80782507543a6cabf4a7a771752664752059142
TiaZheng <71717366+TiaZheng@users.noreply.github.com>
2021-11-18 12:26:51 -0500

    error

--- api-project/src/components/FeaturedPage.vue
+++ api-project/src/components/FeaturedPage.vue
@@ -1,3 +1,36 @@
 <template>
-    <div>helloooooo</div>
+    <div>
+       <ul class="featured-list">
+            <li class="featured-list-item">testingggg</li>
+        </ul>
+    </div>
 </template>
+
+<script>
+export default {
+    name: "FeaturedPage",
+
+    data() {
+        return{
+            animes:[]
+        };
+    },
+
+
+    methods: {
+        fetchAnime: async function(){
+            try{
+                const result = await fetch(
+                    "https://api.jikan.moe/v3/search/anime?order_by=score"
+                );
+                 const data = await result.json();
+                 this.animes = data.results
+                 console.log(data)
+            }
+            catch(error){
+                alert(error)
+            }
+        }
+    }
+}
+</script>
