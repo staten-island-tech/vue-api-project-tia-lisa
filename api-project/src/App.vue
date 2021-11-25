e165562d6eef388bb0b863e69ff6d4621d8d37bb
lisali-liang <lisaliliang27@gmail.com>
2021-11-25 14:09:35 -0500

    11/25

--- api-project/src/App.vue
+++ api-project/src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-
-  <SearchReal class="search-real"></SearchReal>
-  <div class="nav"> 
+    <div class="logo">
+      <h1 class="logo-text">cosmos</h1>
+    </div>
+    <SearchReal class="search-real"></SearchReal>
+    <FeaturedPage></FeaturedPage>
+    <!-- <div class="nav"> 
     <div class="featured">
       <h1>featured</h1>
   <div>
@@ -19,56 +22,55 @@
         <h1>comedy</h1>
       </div>
     </div>
-  </div>
-    <FeaturedPage></FeaturedPage>
-  
+  </div> -->
   </div>
 </template>
 
 <script>
-import FeaturedPage from "./components/FeaturedPage.vue"
+import FeaturedPage from "./components/FeaturedPage.vue";
 import SearchReal from "./components/SearchReal.vue";
 
 export default {
   name: "App",
-  components: { 
+  components: {
     SearchReal,
-    FeaturedPage
+    FeaturedPage,
   },
   data() {
-    return{
-        };
+    return {};
   },
-}
+};
 </script>
 
-
-   
-
 <style>
+@import url("https://fonts.googleapis.com/css2?family=IBM+Plex+Serif:wght@700&display=swap");
+
+:root {
+  --main-font: "IBM Plex Serif", serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  margin: 0;
+  padding: 0;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: rgb(215, 239, 255);
 }
 
-/* #nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
-
-.nav{
+.nav {
   display: flex;
   justify-content: space-around;
+}
+
+h1 {
+  font-family: var(--main-font);
+}
+
+.logo {
+  background-color: #94b3fd;
+  height: 100px;
+}
+.logo-text {
+  color: #99feff;
 }
 </style>
